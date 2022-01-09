44f7494ef2cda7dc3a016114b4a24fc16b1fbe3c
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-01-09 14:18:19 -0600

    Throw exception in StaticUiOffset if width or height is negative

--- src/Rust.UIFramework/Rust.UIFramework/Positions/StaticUiOffset.cs
+++ src/Rust.UIFramework/Rust.UIFramework/Positions/StaticUiOffset.cs
@@ -1,4 +1,6 @@
-﻿namespace UI.Framework.Rust.Positions
+﻿using System;
+
+namespace UI.Framework.Rust.Positions
 {
     public class StaticUiOffset : UiOffset
     {
@@ -6,10 +8,21 @@
 
         public StaticUiOffset(int width, int height) : this(-width / 2, -height / 2, width, height)
         {
+            
         }
 
         public StaticUiOffset(int x, int y, int width, int height)
         {
+            if (width < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(width), "width cannot be less than 0");
+            }
+            
+            if (height < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(height), "height cannot be less than 0");
+            }
+            
             _offset = new Offset(x, y, x + width, y + height);
         }
 
