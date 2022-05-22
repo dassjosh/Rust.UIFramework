563264b63a1ea32e7a3d5b11824a6d0dcd25c709
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-05-22 07:23:24 -0500

    UiImage added CreateFileImage & CreateSpriteImage

--- src/Rust.UIFramework/Rust.UIFramework/UiElements/UiImage.cs
+++ src/Rust.UIFramework/Rust.UIFramework/UiElements/UiImage.cs
@@ -6,11 +6,19 @@ namespace Oxide.Ext.UiFramework.UiElements
 {
     public class UiImage : BaseUiImage
     {
-        public static UiImage Create(UiPosition pos, UiOffset? offset, UiColor color, string png)
+        public static UiImage CreateFileImage(UiPosition pos, UiOffset? offset, UiColor color, string png)
         {
             UiImage image = CreateBase<UiImage>(pos, offset);
             image.Image.Color = color;
             image.Image.Png = png;
+            return image;
+        }
+        
+        public static UiImage CreateSpriteImage(UiPosition pos, UiOffset? offset, UiColor color, string sprite)
+        {
+            UiImage image = CreateBase<UiImage>(pos, offset);
+            image.Image.Color = color;
+            image.Image.Sprite = sprite;
             return image;
         }
     }
