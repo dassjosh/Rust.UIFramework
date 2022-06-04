0057edfa92875ac2a2486f2374768ccecfbd4e1f
Josh Dassinger <dassjosh@users.noreply.github.com>
2022-06-04 07:34:06 -0500

    Added a UIHelpers class Added UiHelpers.CalculateMaxPage that returns the max page

---
+++ src/Rust.UIFramework/Rust.UIFramework/UiHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace Oxide.Ext.UiFramework
+{
+    public static class UiHelpers
+    {
+        public static int CalculateMaxPage(int count, int perPage)
+        {
+            int maxPage = count / perPage + 1;
+            if (count == perPage)
+            {
+                maxPage -= 1;
+            }
+
+            return maxPage;
+        }
+    }
+}
