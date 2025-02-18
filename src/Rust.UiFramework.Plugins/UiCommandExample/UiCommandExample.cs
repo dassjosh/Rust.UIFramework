eafc7af5a1bb4b9b9254ec7294d2c098c07e6f6f
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-02-18 14:52:40 -0600

    Cleanup UiCommandExample

--- src/Rust.UiFramework.Plugins/UiCommandExample/UiCommandExample.cs
+++ src/Rust.UiFramework.Plugins/UiCommandExample/UiCommandExample.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using Oxide.Ext.UiFramework.Builder.UI;
 using Oxide.Ext.UiFramework.Colors;
+using Oxide.Ext.UiFramework.Libraries;
 using Oxide.Ext.UiFramework.Libraries.UiCommands;
 using Oxide.Ext.UiFramework.Positions;
 
 
 namespace Oxide.Plugins;
 
-[Info("Test", "MJSU", "1.0.0")]
-[Description("Test")]
+[Info("UiCommandExample", "MJSU", "1.0.0")]
+[Description("UiCommandExample")]
 public class UiCommandExample : RustPlugin
 {
     private readonly UiCommands _uiCommands = GetLibrary<UiCommands>();
+    private readonly UiPlayerStore _playerStore = GetLibrary<UiPlayerStore>();
 
-    private ICommandBuilder<int, bool, BuildingPrivlidge, DateTime> _doTheThingBuilder;
+    private ICommandBuilder<UiState, MyCustomArg, int, bool, BuildingPrivlidge, DateTime> _doTheThingBuilder;
     
     private void Init()
     {
-        _doTheThingBuilder = _uiCommands.RegisterCommand<int, bool, BuildingPrivlidge, DateTime>(this, HandleDoTheThing);
+        _doTheThingBuilder = _uiCommands.RegisterCommand<UiState, MyCustomArg, int, bool, BuildingPrivlidge, DateTime>(this, HandleDoTheThing);
         _uiCommands.RegisterPlayerCooldownCallback(this, (player, method, cooldown, remaining) => //[Optional]
         {
             //Show UI about being on cooldown
@@ -33,39 +35,61 @@ public class UiCommandExample : RustPlugin
         _uiCommands.RegisterCustomParser(this, new MyCustomClassHandler());
     }
 
-    public void CreateUi(BasePlayer player, BuildingPrivlidge priv)
+    public void CreateUi(BasePlayer player)
+    {
+        UiState state = _playerStore.GetOrCreateStore<UiState>(this, player);
+        CreateUi(player, state, player.GetBuildingPrivilege());
+    }
+    
+    public void CreateUi(BasePlayer player, UiState state, BuildingPrivlidge priv)
     {
         UiBuilder builder = UiBuilder.Create(UiPosition.Full, default, UiColor.White, "UiName");
-        builder.CommandButton(builder.Root, UiPosition.Full, default, UiColor.Clear, _doTheThingBuilder.Build(1, false, priv, DateTime.Now));
+        builder.CommandButton(builder.Root, UiPosition.Full, default, UiColor.Clear, _doTheThingBuilder.Build(state, new MyCustomArg(ulong.MaxValue), 1, false, priv, DateTime.Now));
         builder.AddUi(player);
     }
     
     [UiCommand]
-    [UiProtection(ProtectionType.Simple)]
+    [UiProtection(ProtectionType.Simple, 3600f)]
     [UiCooldown(1f)]
-    [UiPermission(permissions: ["test.admin"])]
-    public void HandleDoTheThing(BasePlayer player, int a, bool b, BuildingPrivlidge id, DateTime c)
+    [UiPermission(permissions: ["test.admin"], PermissionMode.RequireAll)]
+    public void HandleDoTheThing(BasePlayer player, UiState state, MyCustomArg custom, int a, bool b, BuildingPrivlidge id, DateTime c)
     {
         
     }
 
-    private class MyCustomClass
+    public class MyCustomArg(ulong id)
     {
-        public ulong Id { get; set; }
+        public ulong Id { get; set; } = id;
     }
 
-    private class MyCustomClassHandler : IArgHandler<MyCustomClass>
+    private class MyCustomClassHandler : IArgHandler<MyCustomArg>
     {
-        public MyCustomClass Read(ReadOnlySpan<char> arg)
+        private readonly Hash<ulong, MyCustomArg> _classes = new();
+        
+        public MyCustomArg Read(ReadOnlySpan<char> arg)
         {
             ulong id = ulong.Parse(arg);
-            //Lookup by ID and return value
-            return default;
+            if (!_classes.TryGetValue(id, out MyCustomArg custom))
+            {
+                _classes[id] = custom = new MyCustomArg(id);
+            }
+
+            return custom;
         }
 
-        public void Write(UiArgWriter writer, MyCustomClass arg)
+        public void Write(UiArgWriter writer, MyCustomArg arg)
         {
             writer.AppendArg(arg.Id);
+        }
+    }
+
+    public class UiState : IPlayerStore
+    {
+        public ulong PlayerId { get; set; }
+
+        public UiState(ulong playerId)
+        {
+            PlayerId = playerId;
         }
     }
 }
