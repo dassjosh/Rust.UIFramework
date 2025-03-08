88c407b5145bf190c436b08e8c3ce5bf61a84e7a
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-03-08 07:56:49 -0600

    Changed UiCheckbox to be a checkbox. Added UiSwitch using the UiCheckbox icons.

--- src/Rust.UIFramework/Controls/UiSwitch.cs
+++ src/Rust.UIFramework/Controls/UiSwitch.cs
@@ -1,6 +1,5 @@
 ﻿using Oxide.Ext.UiFramework.Builder;
 using Oxide.Ext.UiFramework.Colors;
-using Oxide.Ext.UiFramework.Libraries;
 using Oxide.Ext.UiFramework.Offsets;
 using Oxide.Ext.UiFramework.Positions;
 using Oxide.Ext.UiFramework.UiElements;
@@ -8,25 +7,18 @@ using Rust.UI;
 
 namespace Oxide.Ext.UiFramework.Controls;
 
-public class UiCheckbox : BaseUiControl
+public class UiSwitch : BaseUiControl
 {
     public bool IsChecked;
     public UiButton Button;
     public UiIcon Icon;
         
-    public static UiCheckbox CreateCheckbox(BaseUiBuilder builder, in UiReference parent, in UiPosition pos, in UiOffset offset, bool isChecked, string command, UiColor? checkedColor, UiColor? uncheckedColor, UiColor? buttonColor)
+    public static UiSwitch CreateCheckbox(BaseUiBuilder builder, in UiReference parent, in UiPosition pos, in UiOffset offset, bool isChecked, string command, UiColor? checkedColor, UiColor? uncheckedColor, UiColor? buttonColor)
     {
-        UiCheckbox control = CreateControl<UiCheckbox>();
+        UiSwitch control = CreateControl<UiSwitch>();
         control.IsChecked = isChecked;
         control.Button = builder.CommandButton(parent, pos, offset, buttonColor ?? UiColor.Clear, command);
-        if (isChecked)
-        {
-            control.Icon = builder.Icon(control.Button, UiPosition.Full, default, Icons.CheckSquare, checkedColor ?? UiColors.Rust.Green);
-        }
-        else
-        {
-            control.Icon = builder.Icon(control.Button, UiPosition.Full, default, FontAwesomeRegularIcons.Square, uncheckedColor ?? UiColors.Rust.Red);
-        }
+        control.Icon = builder.Icon(control.Button, UiPosition.Full, default, isChecked ? Icons.ToggleOn : Icons.ToggleOff, isChecked ? checkedColor ?? UiColors.Rust.Green : uncheckedColor ?? UiColors.Rust.Red);
 
         //control.Button.AddOutline(UiColor.Black.WithAlpha(0.75f));
         return control;
