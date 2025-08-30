85f9bfede29672c7439f0b2523ea87ac2f59ec6a
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-08-30 09:57:05 -0500

    Fixed SingletonBehavior having the GameObject being destroyed due to recent rust changes

--- src/Rust.UIFramework/Types/SingletonBehavior.cs
+++ src/Rust.UIFramework/Types/SingletonBehavior.cs
@@ -4,5 +4,14 @@ namespace Oxide.Ext.UiFramework.Types;
 
 internal static class SingletonBehavior<T> where T : FacepunchBehaviour
 {
-    public static readonly T Instance = new GameObject($"{UiFrameworkExtension.Instance.Name} {typeof(T).Name}").AddComponent<T>();
+    // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+    private static readonly GameObject _go;
+    public static readonly T Instance;
+
+    static SingletonBehavior()
+    {
+        _go = new GameObject($"{UiFrameworkExtension.Instance.Name} {typeof(T).Name}");
+        Instance = _go.AddComponent<T>();
+        Object.DontDestroyOnLoad(_go);
+    }
 }
