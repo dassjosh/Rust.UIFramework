5c851c0de821c5381cf06a8572db4e4c61d4e89a
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-09-06 08:14:53 -0500

    More file scopped namespace

--- src/Rust.UIFramework/Exceptions/Pooling/InvalidPoolException.cs
+++ src/Rust.UIFramework/Exceptions/Pooling/InvalidPoolException.cs
@@ -2,34 +2,33 @@
 using System.Runtime.CompilerServices;
 using Oxide.Ext.UiFramework.Pooling;
 
-namespace Oxide.Ext.UiFramework.Exceptions
+namespace Oxide.Ext.UiFramework.Exceptions;
+
+/// <summary>
+/// An exception when something is invalid with a pool
+/// </summary>
+public class InvalidPoolException : BaseUiFrameworkException
 {
-    /// <summary>
-    /// An exception when something is invalid with a pool
-    /// </summary>
-    public class InvalidPoolException : BaseUiFrameworkException
+    private InvalidPoolException(string message) : base(message) {}
+
+    internal static void ThrowIfInvalidPoolSize(PoolSize size)
     {
-        private InvalidPoolException(string message) : base(message) {}
-
-        internal static void ThrowIfInvalidPoolSize(PoolSize size)
+        if (!size.IsValid)
         {
-            if (!size.IsValid)
-            {
-                throw new InvalidPoolException($"Pool Size is not a valid size. Starting: {size.StartingSize} Max: {size.MaxSize}");
-            }
+            throw new InvalidPoolException($"Pool Size is not a valid size. Starting: {size.StartingSize} Max: {size.MaxSize}");
         }
+    }
 
-        internal static void ThrowIfNotPowerOf2(int value, [CallerArgumentExpression("value")] string valueName = null)
+    internal static void ThrowIfNotPowerOf2(int value, [CallerArgumentExpression("value")] string valueName = null)
+    {
+        if (!IsPowerOfTwo(value))
         {
-            if (!IsPowerOfTwo(value))
-            {
-                throw new InvalidPoolException($"Pool Size {valueName}: {value} is not a valid power of 2");
-            }
+            throw new InvalidPoolException($"Pool Size {valueName}: {value} is not a valid power of 2");
         }
+    }
 
-        private static bool IsPowerOfTwo(int x)
-        {
-            return x > 0 && (x & (x - 1)) == 0;
-        }
+    private static bool IsPowerOfTwo(int x)
+    {
+        return x > 0 && (x & (x - 1)) == 0;
     }
 }
