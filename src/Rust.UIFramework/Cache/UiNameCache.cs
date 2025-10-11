55aa56e5bed87e1df72ed185d054f1afef604450
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-10-11 13:42:45 -0500

    Simplified UiNameCache obsolete message

--- src/Rust.UIFramework/Cache/UiNameCache.cs
+++ src/Rust.UIFramework/Cache/UiNameCache.cs
@@ -6,16 +6,18 @@ using Oxide.Ext.UiFramework.Interfaces;
 
 namespace Oxide.Ext.UiFramework.Cache;
 
-[Obsolete($"Create a new class that inherits from {nameof(INamingCache)} and register your stratagy in instance of {nameof(UiBuilder)} calling builder.{nameof(UiBuilder.SetNamingCache)}")]
+[Obsolete(ObsoleteMessage)]
 public static class UiNameCache
 {
+    private const string ObsoleteMessage = $"Create a new class that inherits from {nameof(INamingCache)} and register your stratagy in an instance of {nameof(UiBuilder)} calling builder.{nameof(UiBuilder.SetNamingCache)}";
+    
     private static readonly Dictionary<string, List<string>> ComponentNameCache = new();
     private static readonly Dictionary<string, List<string>> AnchorNameCache = new();
 
-    [Obsolete($"Create a new class that inherits from {nameof(INamingCache)} and register your stratagy in instance of {nameof(UiBuilder)} calling builder.{nameof(UiBuilder.SetNamingCache)}")]
+    [Obsolete(ObsoleteMessage)]
     public static string GetComponentName(string baseName, int index) => GetName(ComponentNameCache, baseName, "_", index);
     
-    [Obsolete($"Create a new class that inherits from {nameof(INamingCache)} and register your stratagy in instance of {nameof(UiBuilder)} calling builder.{nameof(UiBuilder.SetNamingCache)}")]
+    [Obsolete(ObsoleteMessage)]
     public static string GetAnchorName(string baseName, int index) => GetName(AnchorNameCache, baseName, "_anchor_", index);
         
     private static string GetName(Dictionary<string, List<string>> cache, string baseName, string splitter, int index)
