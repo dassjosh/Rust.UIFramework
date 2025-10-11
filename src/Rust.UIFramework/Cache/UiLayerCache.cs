93d7af1a2086fffb9f692f8894de4893894fbca9
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-10-11 13:43:19 -0500

    UiLayers are now extracted during Asset Dump

--- src/Rust.UIFramework/Cache/UiLayerCache.cs
+++ src/Rust.UIFramework/Cache/UiLayerCache.cs
@@ -1,40 +1,34 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using Oxide.Ext.UiFramework.Constants;
 using Oxide.Ext.UiFramework.Enums;
 
 namespace Oxide.Ext.UiFramework.Cache;
 
 public static class UiLayerCache
 {
-    public const string Overall = "Overall";
-    public const string OverlayNonScaled = "OverlayNonScaled";
-    public const string Overlay = "Overlay";
-    public const string Hud = "Hud";
-    public const string HudMenu = "Hud.Menu";
-    public const string Under = "Under";
-    public const string UnderNonScaled = "UnderNonScaled";
-    public const string Inventory = "Inventory";
-    public const string Crafting = "Crafting";
-    public const string Contacts = "Contacts";
-    public const string Clans = "Clans";
-    public const string TechTree = "TechTree";
-    public const string Map = "Map";
-
-    private static readonly ConcurrentDictionary<UiLayer, string> Layers = new()
+    private static readonly ConcurrentDictionary<UiLayer, string> Layers = new();
+    static UiLayerCache()
     {
-        [UiLayer.Overall] = Overall,
-        [UiLayer.Overlay] = Overlay,
-        [UiLayer.OverlayNonScaled] = OverlayNonScaled,
-        [UiLayer.Hud] = Hud,
-        [UiLayer.HudMenu] = HudMenu,
-        [UiLayer.Under] = Under,
-        [UiLayer.UnderNonScaled] = UnderNonScaled,
-        [UiLayer.Inventory] = Inventory,
-        [UiLayer.Crafting] = Crafting,
-        [UiLayer.Contacts] = Contacts,
-        [UiLayer.Clans] = Clans,
-        [UiLayer.TechTree] = TechTree,
-        [UiLayer.Map] = Map,
-    };
+        Type stringType = typeof(string);
+        foreach (FieldInfo field in typeof(UiLayers).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy))
+        {
+            if (field.IsLiteral && !field.IsInitOnly && field.FieldType == stringType)
+            {
+                string enumName = field.Name;
+                string value = (string)field.GetRawConstantValue();
+                if (Enum.TryParse(enumName, out UiLayer layer))
+                {
+                    Layers[layer] = value;
+                }
+                else
+                {
+                    OxideLibrary.LogWarning($"Failed to parse UiLayer enum: {enumName} Value: {value}");
+                }
+            }
+        }
+    }
 
     public static string GetLayer(UiLayer layer)
     {
