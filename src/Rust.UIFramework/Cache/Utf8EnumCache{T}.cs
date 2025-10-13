c6d09b2e239c00da10c114271f66f43f9c4bd0ae
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-10-13 07:20:51 -0500

    Utf8EnumCache{T} improvements

--- src/Rust.UIFramework/Cache/Utf8EnumCache{T}.cs
+++ src/Rust.UIFramework/Cache/Utf8EnumCache{T}.cs
@@ -2,28 +2,35 @@
 using System.Linq;
 using System.Runtime.CompilerServices;
 using Oxide.Ext.UiFramework.Types;
-using Unity.Collections.LowLevel.Unsafe;
 
 namespace Oxide.Ext.UiFramework.Cache;
 
 #pragma warning disable S2743 // Static is not shared across generics
 #pragma warning disable S3963 // Static constructor
 
-internal static class Utf8EnumCache<T> where T : struct, Enum
+internal static class Utf8EnumCache<T> where T : unmanaged, Enum
 {
     private static readonly Utf8String[] Utf8Strings;
 
     static Utf8EnumCache()
     {
+        if (Enum.GetUnderlyingType(typeof(T)) != typeof(byte))
+        {
+            throw new Exception($"Cannot use enum {typeof(T).Name} with FastEnumCache<T> enum underlying type must be byte.");
+        }
+        
         T[] values = Enum.GetValues(typeof(T)).Cast<T>().ToArray();
         Utf8Strings = new Utf8String[values.Length];
         
         foreach (T value in values)
         {
-            Utf8Strings[UnsafeUtility.EnumToInt(value)] = value.ToString("D");
+            Utf8Strings[GetIndex(value)] = value.ToString("D");
         }
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Utf8String ToUtf8Number(T value) => Utf8Strings[UnsafeUtility.EnumToInt(value)];
+    public static Utf8String ToUtf8Number(T value) => Utf8Strings[GetIndex(value)];
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static unsafe int GetIndex(T value) => *(int*)&value;
 }
