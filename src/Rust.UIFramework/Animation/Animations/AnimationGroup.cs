f4772e695016c6f649f6135eca24f45be06c7bb8
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-10-24 12:05:40 -0500

    Setup AnimationGroup on IAnimationBuilder extension methods

--- src/Rust.UIFramework/Animation/Animations/AnimationGroup.cs
+++ src/Rust.UIFramework/Animation/Animations/AnimationGroup.cs
@@ -1,7 +1,11 @@
-﻿namespace Oxide.Ext.UiFramework.Animation;
+﻿using Oxide.Ext.UiFramework.Plugins;
+
+namespace Oxide.Ext.UiFramework.Animation;
 
 public class AnimationGroup : SendableAnimation, IAnimationGroup
 {
     public void AddAnimation(ISendableAnimation animation) => AddChildAnimation((BaseAnimation)animation);
     public void RemoveAnimation(ISendableAnimation animation) => RemoveChildAnimation((BaseAnimation)animation);
+
+    public static AnimationGroup Create(IUiFrameworkPlugin plugin) => plugin.PluginPool.Get<AnimationGroup>();
 }
--- src/Rust.UIFramework/Extensions/AnimationBuilderExt.cs
+++ src/Rust.UIFramework/Extensions/AnimationBuilderExt.cs
@@ -9,9 +9,23 @@ namespace Oxide.Ext.UiFramework.Extensions;
 
 public static class AnimationBuilderExt
 {
+    public static IAnimationGroup AnimateGroup(this IAnimationBuilder builder)
+    {
+        AnimationGroup group = AnimationGroup.Create(builder.Plugin);
+        builder.AddAnimation(group);
+        return group;
+    }
+    
     public static IElementAnimation<T> Animate<T>(this IAnimationBuilder builder, T element) where T : BaseUiComponent, new()
     {
         ElementAnimation<T> animation = ElementAnimation<T>.Create(builder.Plugin, element);
+        builder.AddAnimation(animation);
+        return animation;
+    }
+    
+    public static IElementAnimation<T> Animate<T>(this IAnimationBuilder builder, string name) where T : BaseUiComponent, new()
+    {
+        ElementAnimation<T> animation = ElementAnimation<T>.Create(builder.Plugin, name);
         builder.AddAnimation(animation);
         return animation;
     }
