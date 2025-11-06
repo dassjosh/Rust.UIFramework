d3fa3e6b67f40de4ce8a3415be83d53ab57b4bab
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-11-06 11:22:28 -0600

    Cache UiLerp methods into a dictionary.

--- src/Rust.UIFramework/Animation/Animator/UiLerp.cs
+++ src/Rust.UIFramework/Animation/Animator/UiLerp.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using Oxide.Ext.UiFramework.Colors;
-using Oxide.Ext.UiFramework.Extensions;
 using Oxide.Ext.UiFramework.Offsets;
 using Oxide.Ext.UiFramework.Positions;
 using Oxide.Ext.UiFramework.Types;
@@ -11,41 +11,30 @@ public delegate T UiLerp<T>(T start, T end, float progress);
 
 public static class UiLerp
 {
+    private static readonly Dictionary<Type, object> Cache = new()
+    {
+        [typeof(UiPosition)] = (UiLerp<UiPosition>)UiPosition.LerpUnclamped,
+        [typeof(UiOffset)] = (UiLerp<UiOffset>)UiOffset.LerpUnclamped,
+        [typeof(UiColor)] = (UiLerp<UiColor>)UiColor.Lerp,
+        [typeof(UiRotation)] = (UiLerp<UiRotation>)UiRotation.LerpUnclamped,
+        [typeof(UiScale)] = (UiLerp<UiScale>)UiScale.Lerp,
+        [typeof(UiPadding)] = (UiLerp<UiPadding>)UiPadding.Lerp,
+        [typeof(string)] = (UiLerp<string>)LevenshteinDistanceExt.Lerp,
+        [typeof(sbyte)] = (UiLerp<sbyte>)((start, end, t) => (sbyte)(start + (end - start) * t)),
+        [typeof(byte)] = (UiLerp<byte>)((start, end, t) => (byte)(start + (end - start) * t)),
+        [typeof(short)] = (UiLerp<short>)((start, end, t) => (short)(start + (end - start) * t)),
+        [typeof(ushort)] = (UiLerp<ushort>)((start, end, t) => (ushort)(start + (end - start) * t)),
+        [typeof(int)] = (UiLerp<int>)((start, end, t) => (int)(start + (end - start) * t)),
+        [typeof(uint)] = (UiLerp<uint>)((start, end, t) => (uint)(start + (end - start) * t)),
+        [typeof(long)] = (UiLerp<long>)((start, end, t) => (long)(start + (end - start) * t)),
+        [typeof(ulong)] = (UiLerp<ulong>)((start, end, t) => (ulong)(start + (end - start) * t)),
+        [typeof(float)] = (UiLerp<float>)((start, end, t) => start + (end - start) * t),
+        [typeof(double)] = (UiLerp<double>)((start, end, t) => start + (end - start) * t),
+        [typeof(decimal)] = (UiLerp<decimal>)((start, end, t) => start + (end - start) * (decimal)t)
+    };
+        
     public static UiLerp<T> GetDefault<T>()
     {
-        Type type = typeof(T);
-        if (type == typeof(UiPosition)) return (UiLerp<T>)(object)(UiLerp<UiPosition>)UiPosition.LerpUnclamped;
-        if (type == typeof(UiOffset)) return (UiLerp<T>)(object)(UiLerp<UiOffset>)UiOffset.LerpUnclamped;
-        if (type == typeof(UiColor)) return (UiLerp<T>)(object)(UiLerp<UiColor>)UiColor.Lerp;
-        if (type == typeof(UiRotation)) return (UiLerp<T>)(object)(UiLerp<UiRotation>)UiRotation.LerpUnclamped;
-        if (type == typeof(UiScale)) return (UiLerp<T>)(object)(UiLerp<UiScale>)UiScale.Lerp;
-        if (type == typeof(UiPadding)) return (UiLerp<T>)(object)(UiLerp<UiPadding>)UiPadding.Lerp;
-        if (type == typeof(string)) return (UiLerp<T>)(object)(UiLerp<string>)LevenshteinDistanceExt.Lerp;
-        switch (type.GetTypeCode())
-        {
-            case TypeCode.SByte:
-                return (UiLerp<T>)(object)(UiLerp<sbyte>) (static (start, end, t) => (sbyte)(start + (end - start) * t));
-            case TypeCode.Byte:
-                return (UiLerp<T>)(object)(UiLerp<byte>) (static (start, end, t) => (byte)(start + (end - start) * t));
-            case TypeCode.Int16:
-                return (UiLerp<T>)(object)(UiLerp<short>) (static (start, end, t) => (short)(start + (end - start) * t));
-            case TypeCode.UInt16:
-                return (UiLerp<T>)(object)(UiLerp<ushort>) (static (start, end, t) => (ushort)(start + (end - start) * t));
-            case TypeCode.Int32:
-                return (UiLerp<T>)(object)(UiLerp<int>) (static (start, end, t) => (int)(start + (end - start) * t));
-            case TypeCode.UInt32:
-                return (UiLerp<T>)(object)(UiLerp<uint>) (static (start, end, t) => (uint)(start + (end - start) * t));
-            case TypeCode.Int64:
-                return (UiLerp<T>)(object)(UiLerp<long>) (static (start, end, t) => (long)(start + (end - start) * t));
-            case TypeCode.UInt64:
-                return (UiLerp<T>)(object)(UiLerp<ulong>) (static (start, end, t) => (ulong)(start + (end - start) * t));
-            case TypeCode.Single:
-                return (UiLerp<T>)(object)(UiLerp<float>) (static (start, end, t) => start + (end - start) * t);
-            case TypeCode.Double:
-                return (UiLerp<T>)(object)(UiLerp<double>) (static (start, end, t) => start + (end - start) * t);
-            case TypeCode.Decimal:
-                return (UiLerp<T>)(object)(UiLerp<decimal>) (static (start, end, t) => start + (end - start) * (decimal)t);
-        }
-        return null;
+        return Cache.TryGetValue(typeof(T), out object cached) ? (UiLerp<T>)cached : null;
     }
 }
