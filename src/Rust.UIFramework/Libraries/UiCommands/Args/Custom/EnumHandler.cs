1c337644b2bc52c0ca723ed752aea8edfae0447b
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-12-05 09:11:45 -0600

    Cache type on EnumHandler

--- src/Rust.UIFramework/Libraries/UiCommands/Args/Custom/EnumHandler.cs
+++ src/Rust.UIFramework/Libraries/UiCommands/Args/Custom/EnumHandler.cs
@@ -6,6 +6,7 @@ namespace Oxide.Ext.UiFramework.Libraries;
 internal class EnumHandler<T> : IArgHandler<T>
 {
     private readonly bool _isUnsigned;
+    private readonly Type _enumType = typeof(T);
 
     public EnumHandler()
     {
@@ -13,7 +14,7 @@ internal class EnumHandler<T> : IArgHandler<T>
         _isUnsigned = underlyingType == typeof(byte) || underlyingType == typeof(ushort) || underlyingType == typeof(uint) || underlyingType == typeof(ulong);
     }
     
-    public T Read(ReadOnlySpan<char> arg) => _isUnsigned ? (T)Enum.ToObject(typeof(T), ulong.Parse(arg)) : (T)Enum.ToObject(typeof(T), long.Parse(arg));
+    public T Read(ReadOnlySpan<char> arg) => _isUnsigned ? (T)Enum.ToObject(_enumType, ulong.Parse(arg)) : (T)Enum.ToObject(_enumType, long.Parse(arg));
 
     public void Write(UiArgWriter writer, T arg) => writer.Append(InternalEnumCache<T>.ToNumber(arg));
 }
