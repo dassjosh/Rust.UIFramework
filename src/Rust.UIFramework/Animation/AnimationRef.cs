393581e60e5458e9c76219927a58b06aa836d2c6
Josh Dassinger <dassjosh@users.noreply.github.com>
2025-12-21 11:45:35 -0600

    Fix issues with AnimationRef

--- src/Rust.UIFramework/Animation/AnimationRef.cs
+++ src/Rust.UIFramework/Animation/AnimationRef.cs
@@ -4,12 +4,19 @@ using Oxide.Ext.UiFramework.Plugins;
 
 namespace Oxide.Ext.UiFramework.Animation;
 
-public readonly record struct AnimationRef<T>(T Animation) where T : class, IAnimation
+public readonly record struct AnimationRef<T> where T : class, IAnimation
 {
-    public readonly AnimationId Id = Animation.Id.IsValid ? Animation.Id : throw new AnimationException("Animation is no longer valid.");
+    private readonly T _animation;
+    public readonly AnimationId Id;
 
-    public T Animation { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => IsValid ? Animation : throw new AnimationException($"Animation ID: {Id} is no longer valid."); } 
-    public bool IsValid { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => Animation is not null && Animation.Id == Id; }
+    public AnimationRef(T animation)
+    {
+        _animation = animation;
+        Id = animation.Id.IsValid ? animation.Id : throw new AnimationException("Animation is no longer valid.");
+    }
+
+    public T Animation { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => IsValid ? _animation : throw new AnimationException($"Animation ID: {Id} is no longer valid."); } 
+    public bool IsValid { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => _animation is not null && _animation.Id == Id; }
     public IUiFrameworkPlugin Plugin { [MethodImpl(MethodImplOptions.AggressiveInlining)] get => Animation.Plugin; }
     internal TimingFunction TimingFunction => IsValid ? Animation.Timing : TimingFunctions.Linear;
 
@@ -47,5 +54,10 @@ public readonly record struct AnimationRef<T>(T Animation) where T : class, IAni
         {
             Animation.TimeoutAnimation();
         }
+    }
+
+    public void Deconstruct(out T animation)
+    {
+        animation = Animation;
     }
 }
