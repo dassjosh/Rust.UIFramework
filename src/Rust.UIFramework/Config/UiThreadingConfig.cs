75a6bee6934437557707119b4100e25af34cdf92
Josh Dassinger <dassjosh@users.noreply.github.com>
2026-01-09 15:29:22 -0600

    Missed config file

---
+++ src/Rust.UIFramework/Config/UiThreadingConfig.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace Oxide.Ext.UiFramework.Config;
+
+internal class UiThreadingConfig
+{
+    [JsonProperty("Enable UI Sending Thread")]
+    public bool EnableUiSendingThread { get; set; }
+}
