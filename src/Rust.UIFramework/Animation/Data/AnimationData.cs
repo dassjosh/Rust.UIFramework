9f985efc447e010ff80bc76e0934ca35df4e2f4d
Josh Dassinger <dassjosh@users.noreply.github.com>
2026-01-09 12:58:40 -0600

    Pass the stored animation ID instead of the animations current ID when removing from Animation Data

--- src/Rust.UIFramework/Animation/Data/AnimationData.cs
+++ src/Rust.UIFramework/Animation/Data/AnimationData.cs
@@ -121,7 +121,7 @@ internal class AnimationData : ISingleton
             if (animation is IPoolable { IsPooled: true } || id != animation.Id)
             {
                 //Somehow the animation was disposed and we didn't remove it.
-                RemoveAnimation(animation.Id);
+                RemoveAnimation(id);
                 continue;
             }
 
@@ -130,7 +130,7 @@ internal class AnimationData : ISingleton
                 case AnimationState.Cancelled:
                 case AnimationState.Completed:
                 case AnimationState.Timeout:
-                    RemoveAnimation(animation.Id);
+                    RemoveAnimation(id);
                     break;
             }
         }
