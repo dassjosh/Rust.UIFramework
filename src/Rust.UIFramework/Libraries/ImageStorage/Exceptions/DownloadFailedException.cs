410c46d28ec9cbc113f73be326f0d7b60dabd47f
Josh Dassinger <dassjosh@users.noreply.github.com>
2026-06-18 16:30:53 -0500

    Missed File

---
+++ src/Rust.UIFramework/Libraries/ImageStorage/Exceptions/DownloadFailedException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Oxide.Ext.UiFramework.Libraries;
+
+public class DownloadFailedException(HttpStatusCode code, string responseMessage) : BaseImageStorageException($"HTTP Error {code}: {responseMessage}"), IRegisterImageException
+{
+    public override FailureType FailureType => FailureType.DownloadFailed;
+    public HttpStatusCode Code { get; init; } = code;
+    public string ResponseMessage { get; init; } = responseMessage;
+}
