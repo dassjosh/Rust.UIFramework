97632a03382e3cd810c1a728d71cb9e978fe0f02
Josh Dassinger <dassjosh@users.noreply.github.com>
2026-07-19 10:54:01 -0500

    UI Debug cleanup

--- src/Rust.UIFramework/Builder/UiDebug/UiDebugOptions.cs
+++ src/Rust.UIFramework/Builder/UiDebug/UiDebugOptions.cs
@@ -2,17 +2,14 @@
 
 namespace Oxide.Ext.UiFramework.Builder;
 
-public readonly struct UiDebugOptions(string identifier, UiDebugModes modes = UiDebugModes.File)
+public readonly record struct UiDebugOptions(string Identifier, UiDebugModes Mode = UiDebugModes.File)
 {
-    public readonly string Identifier = identifier;
-    public readonly UiDebugModes Mode = modes;
+    [Pure]
+    public UiDebugOptions WithModes(UiDebugModes mode) => this with { Mode = mode };
     
     [Pure]
-    public UiDebugOptions WithModes(UiDebugModes mode) => new(Identifier, mode);
+    public UiDebugOptions NotWithModes(UiDebugModes modes) => this with { Mode = Mode & ~modes };
     
     [Pure]
-    public UiDebugOptions NotWithModes(UiDebugModes modes) => new(Identifier, Mode & ~modes);
-    
-    [Pure]
-    public UiDebugOptions WithIdentifier(string identifier) => new(identifier, Mode);
+    public UiDebugOptions WithIdentifier(string identifier) => this with { Identifier = identifier };
 }
